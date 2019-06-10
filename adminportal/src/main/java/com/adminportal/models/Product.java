84a04c02131eda82354765ba51247dd685d4b4d0
robin-singh-rana <rasinghrobin@gmail.com>
2019-06-10 15:36:37 +0530

    added product model

--- adminportal/src/main/java/com/adminportal/models/Product.java
+++ adminportal/src/main/java/com/adminportal/models/Product.java
@@ -1,10 +1,110 @@
 package com.adminportal.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.web.multipart.MultipartFile;
 
 @Entity
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String title;
+	private String brand;
+	private String category;
+	private boolean active=true;
+	private double listPrice;
+	private double ourPrice;
+	
+	@Column(columnDefinition = "text")
+	private String description;
+	private int inStockNumber;
+	
+	private MultipartFile productImage;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public double getListPrice() {
+		return listPrice;
+	}
+
+	public void setListPrice(double listPrice) {
+		this.listPrice = listPrice;
+	}
+
+	public double getOurPrice() {
+		return ourPrice;
+	}
+
+	public void setOurPrice(double ourPrice) {
+		this.ourPrice = ourPrice;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getInStockNumber() {
+		return inStockNumber;
+	}
+
+	public void setInStockNumber(int inStockNumber) {
+		this.inStockNumber = inStockNumber;
+	}
+
+	public MultipartFile getProductImage() {
+		return productImage;
+	}
+
+	public void setProductImage(MultipartFile productImage) {
+		this.productImage = productImage;
+	}
+	
 }
