066a67651090974d266be98a88261c95ca47796c
robin-singh-rana <rasinghrobin@gmail.com>
2019-06-24 12:10:18 +0530

    Added ShoppingCart entities in adminportal (for consistency)

---
+++ adminportal/src/main/java/com/adminportal/models/CartItem.java
@@ -0,0 +1,99 @@
+package com.adminportal.models;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class CartItem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private int qty;
+	private BigDecimal subTotal;  //i.e price*qty
+	
+	//one product to one cartItem
+	@OneToOne
+	private Product product;
+	
+	@OneToMany(mappedBy = "cartItem")
+	@JsonIgnore
+	List<ProductToCartItem> productToCartItemList; //entity 1 to entity 2 //middle table b/w product and cartItem
+	
+	@ManyToOne
+	@JoinColumn(name = "shopping_cart_id")
+	private ShoppingCart shoppingCart;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+
+	public BigDecimal getSubTotal() {
+		return subTotal;
+	}
+
+	public void setSubTotal(BigDecimal subTotal) {
+		this.subTotal = subTotal;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public List<ProductToCartItem> getProductToCartItemList() {
+		return productToCartItemList;
+	}
+
+	public void setProductToCartItemList(List<ProductToCartItem> productToCartItemList) {
+		this.productToCartItemList = productToCartItemList;
+	}
+
+	public ShoppingCart getShoppingCart() {
+		return shoppingCart;
+	}
+
+	public void setShoppingCart(ShoppingCart shoppingCart) {
+		this.shoppingCart = shoppingCart;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	
+}
