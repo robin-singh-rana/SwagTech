1e3dcd56ef5f12fe754188c06d4badf03b1ba2ee
robin-singh-rana <rasinghrobin@gmail.com>
2019-06-28 11:05:26 +0530

    Added Order Confirmation email backend

--- src/main/java/com/robin/utilities/MailConstructor.java
+++ src/main/java/com/robin/utilities/MailConstructor.java
@@ -3,11 +3,19 @@ package com.robin.utilities;
 
 import java.util.Locale;
 
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+import com.robin.models.Order;
 import com.robin.models.User;
 
 @Component
@@ -15,6 +23,9 @@ public class MailConstructor {
 
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	private TemplateEngine templateEngine;
 	
 	public SimpleMailMessage constructResetTokenEmail(String contextPath, Locale locale, String token, User user, String password)
 	{
@@ -26,5 +37,29 @@ public class MailConstructor {
 		email.setText(url+message);
 		email.setFrom(env.getProperty("support.email"));
 		return email;
+	}
+
+	public MimeMessagePreparator constructOrderConfirmationEmail(User user, Order order, Locale locale) {
+		
+		Context context = new Context();
+		context.setVariable("order", order);
+		context.setVariable("user",user);
+		context.setVariable("cartItemList", order.getCartItemList());
+		String text = templateEngine.process("orderConfirmationEmailTemplate", context);
+		
+		MimeMessagePreparator mimeMessagePreparator = new MimeMessagePreparator() {
+			
+			@Override
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+				
+				MimeMessageHelper email = new MimeMessageHelper(mimeMessage);
+				email.setTo(user.getEmail());
+				email.setSubject("Order Confirmation - "+order.getId());
+				email.setText(text,true);
+				email.setFrom(new InternetAddress("swagtechps@gmail.com"));
+			}
+		};
+		
+		return mimeMessagePreparator;
 	}
 }
